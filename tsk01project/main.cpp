679b52af6ee71d82d905fb98a87a8d3cc66c5fb4
Anton Aliaksandrau <antonalex2021@gmail.com>
2026-04-05 19:52:10 +0300

    Add project files.

---
+++ tsk01project/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#define DEFAULT_SIZE 10
+using namespace std;
+
+int main() {
+	int allay[DEFAULT_SIZE];
+
+	for (int i = 0; i < DEFAULT_SIZE; i++) {
+		allay[i] = rand() % 101;
+	}
+
+	for (int i = 0; i < DEFAULT_SIZE; i++) {
+		cout << allay[i] << " ";
+	}
+
+	return 0;
+}
